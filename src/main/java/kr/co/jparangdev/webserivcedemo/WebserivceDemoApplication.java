90749c8a5e5ea4a18adeae749e0f816be3537029
jparangdev <jparangdev@gmail.com>
2022-01-09 23:08:34 +0900

    Initial commit

---
+++ src/main/java/kr/co/jparangdev/webserivcedemo/WebserivceDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jparangdev.webserivcedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserivceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserivceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/jparangdev/webserivcedemo/WebserivceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jparangdev.webserivcedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserivceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
